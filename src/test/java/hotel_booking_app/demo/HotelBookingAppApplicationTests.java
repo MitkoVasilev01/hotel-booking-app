459d6d237234dfa902638e05acac5b4845da4585
MitkoVasilev01 <mitko.200101@abv.bg>
2025-10-02 14:55:51 +0300

    feat: Initial project setup

---
+++ src/test/java/hotel_booking_app/demo/HotelBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package hotel_booking_app.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
