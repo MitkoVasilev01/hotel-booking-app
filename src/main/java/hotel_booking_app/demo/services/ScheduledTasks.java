50d1ac8534fdd42cbc6acc9083ed955976dbd4ed
MitkoVasilev01 <mitko.200101@abv.bg>
2025-12-01 16:07:29 +0200

    Add scheduling and logging

--- src/main/java/hotel_booking_app/demo/services/ScheduledTasks.java
+++ src/main/java/hotel_booking_app/demo/services/ScheduledTasks.java
@@ -1,11 +1,17 @@
 package hotel_booking_app.demo.services;
 
 import hotel_booking_app.demo.repositories.ReservationRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 @Component
 public class ScheduledTasks {
+
+    private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
 
     private final ReservationRepository reservationRepository;
 
@@ -14,8 +20,13 @@ public class ScheduledTasks {
     }
 
     @Scheduled(cron = "0 * * * * *")
-    public void reportCurrentTime() {
+    public void reportCurrentReservationCount() {
         long count = reservationRepository.count();
-        System.out.println("⏰ SCHEDULER: В момента в системата има " + count + " резервации.");
+        logger.info("CRON SCHEDULER: В момента в системата има {} резервации.", count);
+    }
+
+    @Scheduled(fixedRate = 300000)
+    public void performHealthCheck() {
+        logger.info("FIXED RATE SCHEDULER: Системата работи нормално към: " + LocalDateTime.now());
     }
 }
